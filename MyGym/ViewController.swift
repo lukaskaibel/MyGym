389f97cc6159cd2363f2efb26c4790413c88ac43
Lukas <lukas.kaibel@icloud.com>
2020-09-19 12:36:44 +0200

    Initial Commit

---
+++ MyGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGym
+//
+//  Created by Lukas Kaibel on 19.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
